1ed2b3e6c8bc7d4cb894e8095af693e0a4afbbab
Kelly Leach <101753828+Keltonlea@users.noreply.github.com>
2022-05-01 21:58:57 -0700

    basic table set up

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,19 @@
-var currentDay = document.getElementById("#currentDay");
-
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
+
+var colorcode = document.querySelector("colorcode");
+
+// var currentHour = (new Date()).getHours();
+// $('.colorcode')
+//   .each(function(){
+//     var val = parseInt($(this).prop('id'));
+//     if(val > currentHour && val < currentHour + 6){
+//       $(this).css('background-color','Blue');
+//     }else if(val < currentHour && val > currentHour-6){
+//       $(this).css('background-color','Red');
+//     }else if(val === currentHour){
+//       $(this).css('background-color','Green');
+//     }else{
+//       $(this).css('background-color','White');
+//     }
+//   });
