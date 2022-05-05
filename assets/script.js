75a6f4ba3b9b831e4946e9d26d62fcc28a9f0a72
Kelly Leach <101753828+Keltonlea@users.noreply.github.com>
2022-05-05 13:37:01 -0700

    color coded

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,18 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 
-var colorcode = document.querySelector("colorcode");
 
-// var currentHour = (new Date()).getHours();
-// $('.colorcode')
-//   .each(function(){
-//     var val = parseInt($(this).prop('id'));
-//     if(val > currentHour && val < currentHour + 6){
-//       $(this).css('background-color','Blue');
-//     }else if(val < currentHour && val > currentHour-6){
-//       $(this).css('background-color','Red');
-//     }else if(val === currentHour){
-//       $(this).css('background-color','Green');
-//     }else{
-//       $(this).css('background-color','White');
-//     }
-//   });
+var currentHour = (new Date()).getHours();
+$('.colorcode')
+  .each(function(){
+    var val = parseInt($(this).prop('id'));
+    if(val > currentHour && val < currentHour + 6){
+      $(this).css('background-color','lightblue');
+    }else if(val < currentHour && val > currentHour-6){
+      $(this).css('background-color','rgb(229, 112, 112)');
+    }else if(val === currentHour){
+      $(this).css('background-color','lightgreen');
+    }else{
+      $(this).css('background-color','White');
+    }
+  });
