86f224fbe754e5420d794f76a468800de540fd41
Kelly Leach <101753828+Keltonlea@users.noreply.github.com>
2022-05-06 23:07:02 -0700

    this isnt going well

--- assets/script.js
+++ assets/script.js
@@ -2,17 +2,22 @@ var today = moment();
 $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 
 
-var currentHour = (new Date()).getHours();
-$('.colorcode')
+let saveButton = $(".saveBtn");
+
+
+
+var currentHour = moment().format("h");
+
+$('.hour')
   .each(function(){
     var val = parseInt($(this).prop('id'));
-    if(val > currentHour && val < currentHour + 6){
+    if(val > currentHour){
       $(this).css('background-color','lightblue');
-    }else if(val < currentHour && val > currentHour-6){
+    }else if(val < currentHour){
       $(this).css('background-color','rgb(229, 112, 112)');
     }else if(val === currentHour){
       $(this).css('background-color','lightgreen');
-    }else{
-      $(this).css('background-color','White');
     }
   });
+
+
