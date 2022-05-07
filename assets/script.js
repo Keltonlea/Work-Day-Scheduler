c01668c512aba322767c13c92688af82a4510ab4
Kelly Leach <101753828+Keltonlea@users.noreply.github.com>
2022-05-07 15:10:02 -0700

    fixed color code and layout, working on local storage issue

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,45 @@
-var today = moment();
-$("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 
+// Header Date
+$("#currentDay").text(moment().format("dddd, MMM Do"));
 
-let saveButton = $(".saveBtn");
+// current time 
+const currentHour = moment().hour();
+let textContent = $(".row textarea");
 
+// past, present, future function
+$(textContent).each(function() {
 
+    let time = $(this).data("hour");
 
-var currentHour = moment().format("h");
+    if(time < currentHour){
+        $(this).addClass("past")
+    } 
 
-$('.hour')
-  .each(function(){
-    var val = parseInt($(this).prop('id'));
-    if(val > currentHour){
-      $(this).css('background-color','lightblue');
-    }else if(val < currentHour){
-      $(this).css('background-color','rgb(229, 112, 112)');
-    }else if(val === currentHour){
-      $(this).css('background-color','lightgreen');
+    else if (time == currentHour){
+        $(this).addClass("present")
     }
-  });
 
+    else if (time > currentHour){
+        $(this).addClass("future")
+    }
+})
 
+// Save user input to local storage
+$(".saveBtn").on("click", function (){
+
+    var eventTime = $(this).data("hour");
+    var eventInput = $(this).siblings("textarea").val();
+
+    console.log(eventTime);
+    localStorage.setItem(eventTime, eventInput);
+
+});
+
+// display event after refresh on page 
+$(textContent).each(function(){
+    var hour = $(this).data("hour");
+    
+    var description = localStorage.getItem(hour);
+    $(this).val(description);
+
+})
